5469c4ccb526f20079cd3ae3c0fee173ec674587
MaciejOlszewski4 <80749071+MaciejOlszewski4@users.noreply.github.com>
2021-03-23 11:58:51 +0100

    Baza danych

--- Sklep/Sklep/DAL/FilmsContext.cs
+++ Sklep/Sklep/DAL/FilmsContext.cs
@@ -17,5 +17,10 @@ namespace Sklep.DAL
         {
 
         }
+
+        static FilmsContext()
+        {
+            Database.SetInitializer<FilmsContext>(new FilmsInitializer());
+        }
     }
 }
